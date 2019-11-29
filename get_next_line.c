cb5decc77b505f59d15c5244f7ed1ea7e066f5dd
Adrien MADENE <admadene@e2r2p15.42.fr>
2019-11-29 14:59:28 +0100

    gnl version final

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: admadene <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 14:46:17 by admadene          #+#    #+#             */
-/*   Updated: 2019/10/30 14:19:39 by admadene         ###   ########.fr       */
+/*   Updated: 2019/11/29 14:59:05 by admadene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,26 +35,27 @@ int		get_current_line(int fd, char **save)
 
 int		get_next_line(int fd, char **line)
 {
-	static char		*save;
+	static char		*save[1024];
 	char			*tmp;
 
 	if (read(fd, NULL, 0) == -1 || !line || BUFFER_SIZE < 1)
 		return (-1);
-	if (!get_current_line(fd, &save))
+	if (!get_current_line(fd, &save[fd]))
 		return (-1);
-	tmp = save;
-	if (ft_chr(save, '\n'))
+	tmp = save[fd];
+	if (ft_chr(save[fd], '\n'))
 	{
-		if (!(*line = ft_sub(save, 0, (ft_chr(save, '\n') - save))))
+		if (!(*line = ft_sub(save[fd], 0, (ft_chr(save[fd], '\n') - save[fd]))))
 			return (-1);
-		if (!(save = ft_strdup(ft_chr(save, '\n') + 1)))
+		if (!(save[fd] = ft_strdup(ft_chr(save[fd], '\n') + 1)))
 			return (-1);
 		free(tmp);
 		tmp = NULL;
 		return (1);
 	}
-	if (!(*line = ft_strdup(save)))
+	if (!(*line = ft_strdup(save[fd])))
 		return (-1);
-	*tmp = 0;
+	free(tmp);
+	save[fd] = NULL;
 	return (0);
 }
